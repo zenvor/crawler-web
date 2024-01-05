7979d72293a6e88a3e6b1c2fbe06a8edaa0710d2
zenvor <52726419+zenvor@users.noreply.github.com>
2024-01-06 00:49:06 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
-import webfont from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
   resolve: {
