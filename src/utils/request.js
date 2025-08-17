f39c899dcf096707be0cca94b1bc5650dbb8d9ba
zenvor <52726419+zenvor@users.noreply.github.com>
2025-08-18 02:06:51 +0800

    chore(web): 更新请求工具实现与规则占位目录

--- src/utils/request.js
+++ src/utils/request.js
@@ -50,7 +50,7 @@ service.interceptors.response.use(
     } else if (message.includes('timeout')) {
       message = '系统接口请求超时'
     } else if (message.includes('Request failed with status code')) {
-      message = '系统接口' + message.substr(message.length - 3) + '异常'
+      message = '系统接口' + message.slice(-3) + '异常'
     }
     return Promise.reject(error)
   }
